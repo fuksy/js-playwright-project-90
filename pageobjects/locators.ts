42b2ac23676436f7045f3125ed3ee2e665275898
Kate <katerit111@gmail.com>
2025-07-21 23:10:16 +0500

    add tests2

--- pageobjects/locators.ts
+++ pageobjects/locators.ts
@@ -9,11 +9,15 @@
 
     }
     export const userslocators = {
-        usersbutton : '//button[@href="#/users"]',
+        usersbutton : '//a[@href="#/users" and contains(text(), "Users")]',
         usercreatebutton: '//a[@href="#/users/create"]',
         useremailinput: '//input[@name="email"]',
         usernameinput: '//input[@name="firstName"]',
         userlastnameinput: '//input[@name="lastName"]',
         savebutton: '//button[@aria-label="Save"]',
-        creatednotification: '//div[contains(text(),"Element created")]',
+        creatednotification: '//div[@class="MuiSnackbar-root MuiSnackbar-anchorOriginBottomCenter css-cjae3w-MuiSnackbar-root-RaNotification-root"]',
+        chckboxUser: '(//input[@class="PrivateSwitchBase-input css-1m9pwf3"])[2]',
+        deletebutton: '//button[@aria-label="Delete"]',
+        chckboxallUser: '(//input[@class="PrivateSwitchBase-input css-1m9pwf3"])[1]',
+        userelementTable: '//tr[@class="MuiTableRow-root MuiTableRow-hover RaDatagrid-row RaDatagrid-rowEven RaDatagrid-selectable RaDatagrid-clickableRow css-1q1u3t4-MuiTableRow-root"][1]'
     }
